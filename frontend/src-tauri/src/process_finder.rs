a5a20bf0e23d51d22ee14104049ff501e33ecf3d
dadodasyra <dadodasyraminecraftien@gmail.com>
2024-02-25 19:18:51 +0100

    Move to sysinfo API

--- frontend/src-tauri/src/process_finder.rs
+++ frontend/src-tauri/src/process_finder.rs
@@ -1,28 +1,20 @@
-use std::process::Command;
 use std::str;
+use sysinfo::{System};
 
 pub(crate) struct ProcessFinder;
 
 impl ProcessFinder {
     pub fn find_pid_of(process_name: &str) -> Vec<String> {
-        let output = Command::new("tasklist")
-            .args(&["/FI", &format!("IMAGENAME eq {}", process_name)])
-            .output()
-            .expect("Failed to execute command");
-
-        let output_str = String::from_utf8_lossy(&output.stdout);
-
+        let mut system = System::new_all();
         let mut pids = Vec::new();
-        for line in output_str.lines() {
-            if line.contains(process_name) {
-                let parts: Vec<&str> = line.split_whitespace().collect();
-                if parts.len() > 1 {
-                    pids.push(parts[1].to_string());
-                }
+        system.refresh_all();
+
+        for (pid, process) in system.processes() {
+            if process.name().to_lowercase() == process_name.to_lowercase() {
+                println!("Found process: {} with PID: {}", process_name, pid);
+                pids.push(pid.to_string());
             }
         }
-
-        println!("Parsed PIDs: {:?}", pids);
 
         pids
     }
