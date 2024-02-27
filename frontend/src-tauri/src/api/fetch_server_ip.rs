ef98b5bbdeb7edf41c2aee52a5aa631c980fd824
dadodasyra <dadodasyraminecraftien@gmail.com>
2024-02-27 17:40:34 +0100

    Choose automatically default device

--- frontend/src-tauri/src/api/fetch_server_ip.rs
+++ frontend/src-tauri/src/api/fetch_server_ip.rs
@@ -1,10 +1,10 @@
-use pcap::Device;
+use pcap::{ConnectionStatus, Device};
 use std::io::{BufRead, BufReader};
 use std::sync::{Arc, Mutex};
 use std::process::{Command, Stdio};
 use std::thread;
 use std::time::Duration;
-use crate::network::network_tools::{NetworkTools, parse_packets};
+use crate::network::network_tools::parse_packets;
 use crate::network::network_tools::get_capture_result;
 
 pub struct FetchServerIp {
@@ -12,7 +12,7 @@ pub struct FetchServerIp {
     pid: u32,
     pub server_ip: String,
     pub port: u16,
-    is_running: bool
+    is_running: bool,
 }
 
 impl FetchServerIp {
@@ -22,7 +22,7 @@ impl FetchServerIp {
             pid,
             server_ip: String::new(),
             port: 0,
-            is_running: false
+            is_running: false,
         }));
 
         let fetch_server_ip_clone = Arc::clone(&fetch_server_ip);
@@ -34,28 +34,50 @@ impl FetchServerIp {
 
     pub fn update_server_ip(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         let local_destination_port = self.get_dynamic_port_for_pid().ok_or("Failed to get the local destination port for PID")?;
+    pub fn initialize_listeners(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         println!("Local destination port: {}", local_destination_port);
 
-        // Find the default device to capture from
-        let device = Device::lookup()
-            .expect("Unable to get default device");
+        // Get a list of all devices
+        let devices = Device::list().map_err(|_| "Failed to get device list")?;
 
-        let (_device_name, cap_result) = get_capture_result(device.unwrap());
+        //  TODO Find the device based on its status if it's connected by filtering out
+        //  the devices that are not connected
+        let device = devices.iter().find(|d|
+            d.flags.connection_status == ConnectionStatus::Connected
+                && d.flags.is_up()
+                && d.flags.is_running()
+                && !d.addresses.is_empty()).cloned();
 
-        // Handle the cap_result Option
-        let cap = cap_result.ok_or("Capture result not found")?;
-        let current_capture_id = self.current_capture_id.clone();
-        thread::Builder::new()
-            .name("thread_parse_packets".to_string())
-            .spawn(move || {
-                parse_packets(
-                    &current_capture_id,
-                    cap
-                );
-            })
-            .unwrap();
+        println!("Device: {:?}", device.clone().unwrap().desc);
+        //println!("Flags: {:?}", device.as_ref().map(|d| d.flags));
 
-        Ok(())
+        match device {
+            Some(device) => {
+                let (_device_name, cap_result) = get_capture_result(device);
+
+                // Handle the cap_result Option
+                let mut cap = cap_result.ok_or("Capture result not found")?;
+                match cap.filter(&*format!("udp port {}", local_destination_port), true) {
+                    Ok(_) => println!("Filter set successfully"),
+                    Err(e) => eprintln!("Error while setting filter: {}", e),
+                }
+
+                let current_capture_id = self.current_capture_id.clone();
+                thread::Builder::new()
+                    .name("thread_parse_packets".to_string())
+                    .spawn(move || {
+                        parse_packets(
+                            &current_capture_id,
+                            cap,
+                            local_destination_port
+                        );
+                    })
+                    .unwrap();
+
+                Ok(())
+            },
+            None => Err("Device not found".into()),
+        }
     }
 
     fn get_dynamic_port_for_pid(&mut self) -> Option<u16> {
@@ -88,31 +110,31 @@ impl FetchServerIp {
 
 
     fn start_update_thread(&mut self, fetch_server_ip: Arc<Mutex<Self>>) {
-        if self.is_running { return }
+        if self.is_running { return; }
 
         let fetch_server_ip = Arc::clone(&fetch_server_ip);
         thread::spawn(move || {
             //loop {
-                // Lock the mutex before accessing FetchServerIp
-                let mut fetch_server_ip = fetch_server_ip.lock().unwrap();
+            // Lock the mutex before accessing FetchServerIp
+            let mut fetch_server_ip = fetch_server_ip.lock().unwrap();
 
-                // Update the server_ip field with the obtained IP address
-                match fetch_server_ip.update_server_ip() {
-                    Ok(_) => {
-                        println!("Server IP: {}", fetch_server_ip.server_ip);
-                    }
-                    Err(e) => {
-                        eprintln!("An error occurred while updating the server IP: {}", e);
-                    }
-                };
+            // Update the server_ip field with the obtained IP address
+            match fetch_server_ip.initialize_listeners() {
+                Ok(_) => {
+                    println!("Server IP: {}", fetch_server_ip.server_ip);
+                }
+                Err(e) => {
+                    eprintln!("An error occurred while updating the server IP: {}", e);
+                }
+            };
 
-                let duration = match fetch_server_ip.server_ip == "" {
-                    true => Duration::from_millis(1000),
-                    _ => Duration::from_secs(5),
-                };
+            let duration = match fetch_server_ip.server_ip == "" {
+                true => Duration::from_millis(1000),
+                _ => Duration::from_secs(5),
+            };
 
-                // Sleep for a certain duration before the next update
-                thread::sleep(duration);
+            // Sleep for a certain duration before the next update
+            thread::sleep(duration);
             //}
         });
     }
