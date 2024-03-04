7405d1af48983275b14321c2a9c79a5e9da8577b
Alexandre PETIT <alexbreuillet@gmail.com>
2024-03-04 16:35:35 +0100

    Patch user text selection

--- frontend/src/components/fleet/FleetLobby.vue
+++ frontend/src/components/fleet/FleetLobby.vue
@@ -3,10 +3,13 @@
     <BannerTemplate>
       <template #content>
         <div class="header-content">
-          <img src="@/assets/icons/sot.svg">
+          <img src="@/assets/icons/sot.svg" />
           <div class="title-content">
             <p>{{ session.sessionName }}</p>
-            <p class="id">{{ t('session.id') + ": " }} <span>{{ session.sessionId.toUpperCase() }}</span></p>
+            <p class="id">
+              {{ t("session.id") + ": " }}
+              <span>{{ session.sessionId.toUpperCase() }}</span>
+            </p>
           </div>
         </div>
       </template>
@@ -17,32 +20,41 @@
     <div class="lobby-content">
       <div class="player-table">
         <PlayerFleet
-            v-for="player in computedsession.players.sort((a,b)=>{return (a.isMaster === b.isMaster)? 0 : a.isMaster? -1 : 1;})"
-            :player="player"/>
+          v-for="player in computedsession.players.sort((a, b) => {
+            return a.isMaster === b.isMaster ? 0 : a.isMaster ? -1 : 1;
+          })"
+          :player="player"
+        />
       </div>
       <div class="lobby-details">
-        <button :class="{'ready-button':true,'not':!UserStore.player.isReady}" @click="updateStatus">
-          <p v-if="UserStore.player.isReady">{{ t('session.player.ready') }}</p>
-          <p v-else>{{ t('session.player.notReady') }}</p>
+        <button
+          :class="{ 'ready-button': true, not: !UserStore.player.isReady }"
+          @click="updateStatus"
+        >
+          <p v-if="UserStore.player.isReady">{{ t("session.player.ready") }}</p>
+          <p v-else>{{ t("session.player.notReady") }}</p>
         </button>
         <div class="details-content">
           <div class="top-content">
             <div class="header-information">
-              <h2>{{ t('session.informations.title') }}</h2>
+              <h2>{{ t("session.informations.title") }}</h2>
             </div>
 
             <div class="information-data">
-              <h3>{{ t('session.informations.totalPlayer') }}</h3>
+              <h3>{{ t("session.informations.totalPlayer") }}</h3>
               <p>{{ session.players.length }}</p>
             </div>
 
             <div class="information-data important">
-              <h3>{{ t('session.informations.totalPlayer') }}</h3>
-              <p>{{ session.players.filter(x => x.isReady).length }} / <span>{{ session.players.length }}</span></p>
+              <h3>{{ t("session.informations.totalPlayer") }}</h3>
+              <p>
+                {{ session.players.filter((x) => x.isReady).length }} /
+                <span>{{ session.players.length }}</span>
+              </p>
             </div>
           </div>
           <button class="session-status" @click="session.leaveSession()">
-            <p>{{ t('session.leave') }}</p>
+            <p>{{ t("session.leave") }}</p>
           </button>
         </div>
       </div>
@@ -51,34 +63,31 @@
 </template>
 
 <script setup lang="ts">
-
-import {computed, PropType} from "vue";
-import {Fleet} from "@/objects/Fleet.ts";
+import { computed, PropType } from "vue";
+import { Fleet } from "@/objects/Fleet.ts";
 import PlayerFleet from "@/vue/fleet/PlayerFleet.vue";
-import {useI18n} from "vue-i18n";
+import { useI18n } from "vue-i18n";
 import BannerTemplate from "@/vue/templates/BannerTemplate.vue";
-import {UserStore} from "@/objects/stores/UserStore.ts";
+import { UserStore } from "@/objects/stores/UserStore.ts";
 
-const {t} = useI18n()
+const { t } = useI18n();
 const props = defineProps({
   session: {
     type: Object as PropType<Fleet>,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 
 function updateStatus() {
   UserStore.player.isReady = !UserStore.player.isReady;
-  props.session.updateToSession()
+  props.session.updateToSession();
 }
 
 const emits = defineEmits(["update:selected-value"]);
 const computedsession = computed({
   get: (): Fleet => props.session,
-  set: (): void => {
-  },
+  set: (): void => {},
 });
-
 </script>
 
 <style scoped lang="scss">
@@ -111,6 +120,7 @@ const computedsession = computed({
         font-size: 16px;
 
         span {
+          user-select: all;
           color: var(--primary);
         }
       }
@@ -121,10 +131,13 @@ const computedsession = computed({
     all: unset;
     cursor: pointer;
     height: 100%;
-    background: linear-gradient(270deg, rgba(50, 212, 153, 0.20) 0%, rgba(50, 212, 153, 0.00) 108.45%);
+    background: linear-gradient(
+      270deg,
+      rgba(50, 212, 153, 0.2) 0%,
+      rgba(50, 212, 153, 0) 108.45%
+    );
     padding: 0 16px;
   }
-
 
   .lobby-content {
     height: calc(100% - 140px); // Minus header height
@@ -160,7 +173,11 @@ const computedsession = computed({
       .ready-button {
         all: unset;
         border-radius: 5px;
-        background: linear-gradient(0deg, rgba(50, 212, 153, 0.20) -14.61%, rgba(50, 212, 153, 0.07) 167.42%);
+        background: linear-gradient(
+          0deg,
+          rgba(50, 212, 153, 0.2) -14.61%,
+          rgba(50, 212, 153, 0.07) 167.42%
+        );
         width: 100%;
         height: 80px;
         margin-bottom: 8px;
@@ -168,7 +185,11 @@ const computedsession = computed({
         cursor: pointer;
 
         &.not {
-          background: linear-gradient(0deg, rgba(212, 50, 50, 0.20) -14.61%, rgba(212, 50, 50, 0.07) 167.42%);
+          background: linear-gradient(
+            0deg,
+            rgba(212, 50, 50, 0.2) -14.61%,
+            rgba(212, 50, 50, 0.07) 167.42%
+          );
         }
       }
 
@@ -181,10 +202,8 @@ const computedsession = computed({
         justify-content: space-between;
 
         .top-content {
-
-
           .header-information {
-            background: rgba(23, 26, 33, 0.50);
+            background: rgba(23, 26, 33, 0.5);
             padding: 20px 0;
             width: 100%;
 
@@ -194,7 +213,6 @@ const computedsession = computed({
               text-align: center;
             }
           }
-
 
           .information-data {
             display: flex;
@@ -211,7 +229,7 @@ const computedsession = computed({
             }
 
             &.important {
-              background: rgba(50, 212, 153, 0.10);;
+              background: rgba(50, 212, 153, 0.1);
             }
 
             p {
@@ -228,13 +246,14 @@ const computedsession = computed({
           padding: 20px 0;
           width: 100%;
           text-align: center;
-          background: linear-gradient(0deg, rgba(212, 50, 50, 0.20) 0%, rgba(212, 50, 50, 0.00) 97.89%);
+          background: linear-gradient(
+            0deg,
+            rgba(212, 50, 50, 0.2) 0%,
+            rgba(212, 50, 50, 0) 97.89%
+          );
         }
-
       }
     }
-
   }
-
 }
 </style>
