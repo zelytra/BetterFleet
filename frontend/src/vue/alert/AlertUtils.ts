f20e9058bc3a7ca33c11d73eac00773a7934ea40
Alexandre PETIT <alexbreuillet@gmail.com>
2024-03-05 16:41:15 +0100

    Patch traduction for basic error message tools

--- frontend/src/vue/alert/AlertUtils.ts
+++ frontend/src/vue/alert/AlertUtils.ts
@@ -1,37 +1,38 @@
-import { Alert, AlertType } from "@/vue/alert/Alert.ts";
+import {Alert, AlertType} from "@/vue/alert/Alert.ts";
+import i18n from "@/objects/i18n";
+
+const {t} = i18n.global;
 
 export class AlertUtils {
-  public static getErrorAlert(): Alert {
-    return {
-      type: AlertType.ERROR,
-      title: "Erreur",
-      content: "Une erreur est survenue en tentant de contacté le serveur.",
-    };
-  }
+    public static getErrorAlert(): Alert {
+        return {
+            type: AlertType.ERROR,
+            title: t('alert.error.title'),
+            content: t('alert.error.content'),
+        };
+    }
 
-  public static getForbiddenAccessAlert(): Alert {
-    return {
-      type: AlertType.ERROR,
-      title: "Accès refusé",
-      content:
-        "Vous ne disposez pas des autorisations nécessaire pour acceder a cette page.",
-    };
-  }
+    public static getForbiddenAccessAlert(): Alert {
+        return {
+            type: AlertType.ERROR,
+            title: t('alert.forbidden.title'),
+            content: t('alert.forbidden.content'),
+        };
+    }
 
-  public static getBadRequest(): Alert {
-    return {
-      type: AlertType.ERROR,
-      title: "Mauvaise requête",
-      content: "La requête est corrompue ou incorrect.",
-    };
-  }
+    public static getBadRequest(): Alert {
+        return {
+            type: AlertType.ERROR,
+            title: t('alert.badRequest.title'),
+            content: t('alert.badRequest.content'),
+        };
+    }
 
-  public static getUnsupportedMediaType(): Alert {
-    return {
-      type: AlertType.ERROR,
-      title: "Media non supporté",
-      content:
-        "Le media que vous tenté d'envoyer n'est pas supporter par l'application.",
-    };
-  }
+    public static getUnsupportedMediaType(): Alert {
+        return {
+            type: AlertType.ERROR,
+            title: t('alert.unsupportedMedia.title'),
+            content: t('alert.unsupportedMedia.content'),
+        };
+    }
 }
