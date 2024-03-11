ceeb34b6a8d4967d5fced10ebc873808b2a194ee
dadodasyra <44753923+dadodasyra@users.noreply.github.com>
2024-03-11 21:32:22 +0100

    Fix focus while doing something or being on another app (#96)

--- frontend/src-tauri/src/window_interaction.rs
+++ frontend/src-tauri/src/window_interaction.rs
@@ -1,9 +1,6 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 use std::ffi::CString;
 use std::ptr::null_mut;
-use winapi::um::winuser::{FindWindowA, SetForegroundWindow, INPUT, INPUT_KEYBOARD, KEYBDINPUT, KEYEVENTF_KEYUP, SendInput};
+use winapi::um::winuser::{FindWindowA, SetForegroundWindow, INPUT, INPUT_KEYBOARD, KEYBDINPUT, KEYEVENTF_KEYUP, VK_MENU, keybd_event, SendInput, KEYEVENTF_EXTENDEDKEY};
 use winapi::ctypes::c_int;
 use std::mem::size_of;
 
@@ -15,8 +12,12 @@ pub(crate) fn set_focus_to_window(window_name: &str) -> bool {
         println!("Could not find window with name: {}", window_name);
         false
     } else {
-        unsafe { SetForegroundWindow(window_handle) };
-        true
+        //This is a bypass from windows restriction
+        //Holding alt while focusing seems to grant ability to focus in every conditions
+        unsafe { keybd_event(VK_MENU as u8, 0, KEYEVENTF_EXTENDEDKEY | 0, 0); }
+        let result = unsafe { SetForegroundWindow(window_handle) };
+        unsafe { keybd_event(VK_MENU as u8, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0); }
+        return result != 0;
     }
 }
 
