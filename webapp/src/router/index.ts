c7591ae2d28f24939de9b53838b32724bca0c386
Zelytra <72125876+zelytra@users.noreply.github.com>
2024-03-12 23:23:12 +0100

    Username length & Tooltip (#112)

--- webapp/src/router/index.ts
+++ webapp/src/router/index.ts
@@ -5,42 +5,51 @@ import config from "@/assets/icons/config.svg"
 import sot from "@/assets/icons/logo.svg"
 import Fleet from "@/components/Fleet.vue";
 import Config from "@/components/Config.vue";
+import i18n from "@/objects/i18n";
 
 declare module 'vue-router' {
-    interface RouteMeta {
-        icon?: string,
-        role?: string
-    }
+  interface RouteMeta {
+    icon?: string,
+    role?: string,
+    tooltip?: string
+  }
 }
 
+const {t} = i18n.global;
+
 export const routes = [
-    {
-        path: "/",
-        name: "Home",
-        component: Home,
-        meta: {icon: sot}
-    },
-    {
-        path: "/fleet",
-        name: "Fleet",
-        component: Fleet,
-        meta: {
-            icon: fleet
-        }
-    }, {
-        path: "/config",
-        name: "Config",
-        component: Config,
-        meta: {
-            icon: config
-        }
-    },
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: {
+      icon: sot,
+      tooltip: t('tooltips.navbar.home')
+    }
+  },
+  {
+    path: "/fleet",
+    name: "Fleet",
+    component: Fleet,
+    meta: {
+      icon: fleet,
+      tooltip: t('tooltips.navbar.fleet')
+    }
+  }, {
+    path: "/config",
+    name: "Config",
+    component: Config,
+    meta: {
+      icon: config,
+      tooltip: t('tooltips.navbar.config')
+    }
+  },
 
 ];
 
 export const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
