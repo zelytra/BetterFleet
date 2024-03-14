94a838a229e9e022e92e2d9fe5af8741851751b8
Zelytra <contact@zelytra.fr>
2024-03-14 19:58:29 +0100

    Remove source files

--- website/src/main.ts
+++ website/src/main.ts
@@ -3,7 +3,6 @@ import "@/assets/style.scss";
 import "@/assets/font.scss";
 import en from "@/assets/locales/fr.json";
 import fr from "@/assets/locales/fr.json";
-import source from "@/assets/locales/source.json";
 import es from "@/assets/locales/fr.json";
 import de from "@/assets/locales/fr.json";
 import {createI18n} from "vue-i18n";
@@ -15,12 +14,8 @@ export const i18n = createI18n({
   legacy: false, // you must set `false`, to use Composition API
   locale: "fr", // set locale
   fallbackLocale: "en", // set fallback locale
-  messages: {fr, en, es, de, source},
+  messages: {fr, en, es, de},
 });
-
-if(JSON.parse(import.meta.env.VITE_DEBUG)){
-  i18n.global.locale.value = "source"
-}
 
 const app = createApp(App)
 app.use(router);
