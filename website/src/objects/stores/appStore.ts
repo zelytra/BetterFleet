d7f2d7dbd926685558d422bba5f74234b2f20d1d
Zelytra <72125876+zelytra@users.noreply.github.com>
2024-03-20 20:42:44 +0100

    Website lang detection (#175)

--- website/src/objects/stores/appStore.ts
+++ website/src/objects/stores/appStore.ts
@@ -2,6 +2,7 @@ import {reactive} from "vue";
 import {AxiosResponse} from "axios";
 import {GithubRelease} from "@/objects/Github.ts";
 import {HTTPAxios} from "@/objects/HTTPAxios.ts";
+import {i18n} from "@/main.ts";
 
 
 export const AppStore = reactive({
@@ -10,5 +11,7 @@ export const AppStore = reactive({
     new HTTPAxios("github/release/download").get().then((response: AxiosResponse) => {
       this.githubRelease = response.data as GithubRelease
     })
+    const browserLang = navigator.language.substring(0, 2);
+    i18n.global.locale.value = browserLang as ("fr" | "en" | "es" | "de")
   },
 });
