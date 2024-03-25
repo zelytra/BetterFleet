31dad9c0e728a91ca0559d731f91c430c92e3d39
Alexandre PETIT <alexbreuillet@gmail.com>
2024-03-25 15:45:30 +0100

    Finally, configure Keycloak for Microsoft connection

--- webapp/src/objects/stores/LoginStates.ts
+++ webapp/src/objects/stores/LoginStates.ts
@@ -2,26 +2,29 @@ import {reactive} from 'vue'
 import Keycloak, {KeycloakConfig} from "keycloak-js";
 
 let initOptions: KeycloakConfig = {
-    url: import.meta.env.VITE_KEYCLOAK_HOST,
-    realm: 'Betterfleet',
-    clientId: 'application'
+  url: import.meta.env.VITE_KEYCLOAK_HOST,
+  realm: 'Betterfleet',
+  clientId: 'application'
 }
 
 export const keycloakStore = reactive({
-    keycloak: new Keycloak(initOptions),
-    isKeycloakInit: false,
+  keycloak: new Keycloak(initOptions),
+  isKeycloakInit: false,
 
-    init(redirectionUrl: string) {
-        this.keycloak.init({
-            onLoad: 'login-required',
-            checkLoginIframe: false,
-            redirectUri: redirectionUrl
-            //silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
-        }).then((auth) => {
-            console.log(auth)
-            this.isKeycloakInit = auth;
-        }).catch((_error) => {
-        })
-
-    },
+  init(redirectionUrl: string) {
+    this.keycloak.init({
+      onLoad: 'check-sso',
+      checkLoginIframe: false,
+      silentCheckSsoRedirectUri: false,
+      redirectUri: redirectionUrl
+      //silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
+    }).then((auth: boolean) => {
+      this.isKeycloakInit = auth;
+    })
+  },
+  loginUser(redirectionUrl: string) {
+    if (!keycloakStore.isKeycloakInit || !keycloakStore.keycloak.authenticated) {
+      window.open(keycloakStore.keycloak.createLoginUrl({redirectUri: redirectionUrl}), '_self')
+    }
+  }
 })
