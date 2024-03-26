6d62e3b6f5fed31d02e2719294ccd3083233fdcc
Alexandre PETIT <alexbreuillet@gmail.com>
2024-03-26 17:11:38 +0100

    Refacto visually the config page

---
+++ webapp/src/vue/templates/ParameterPart.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="param-wrapper">
+    <h2>{{ title }}</h2>
+    <div class="template-wrapper">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: String
+})
+</script>
+
+<style scoped lang="scss">
+.param-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  gap: 20px;
+
+  .template-wrapper {
+    border: 2px solid #222631;
+    background: rgba(34, 38, 49, 0.20);
+    padding: 43px;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    box-sizing: border-box;
+    border-radius: 5px;
+    gap: 40px;
+  }
+}
+</style>
