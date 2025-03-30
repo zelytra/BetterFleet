ebe963ef894de5da4982c5ca003552f29fa2b534
dadodasyra <44753923+dadodasyra@users.noreply.github.com>
2025-03-30 16:56:20 +0200

    Update index.ts

--- website/src/objects/i18n/index.ts
+++ website/src/objects/i18n/index.ts
@@ -3,10 +3,11 @@ import fr from "@assets/locales/fr.json";
 import en from "@assets/locales/fr.json";
 import es from "@assets/locales/fr.json";
 import de from "@assets/locales/fr.json";
+import source from "@assets/locales/source.json";
 
 export const i18n = createI18n({
   legacy: false, // you must set `false`, to use Composition API
   locale: "fr", // set locale
-  fallbackLocale: "fr", // set fallback locale
-  messages: { fr, en, es, de },
+  fallbackLocale: "source", // set fallback locale
+  messages: { fr, en, es, de, source },
 });
