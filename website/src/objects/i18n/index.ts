890bf931efcd3942d9fc01ab3c54710affd1e2db
dadodasyra <44753923+dadodasyra@users.noreply.github.com>
2025-03-30 17:07:47 +0200

    Update index.ts

--- website/src/objects/i18n/index.ts
+++ website/src/objects/i18n/index.ts
@@ -5,7 +5,7 @@ import es from "@assets/locales/es.json";
 import de from "@assets/locales/de.json";
 import source from "@assets/locales/source.json";
 
-export const i18n = createI18n({
+export const tsi18n = createI18n({
   legacy: false, // you must set `false`, to use Composition API
   locale: "fr", // set locale
   fallbackLocale: "source", // set fallback locale
