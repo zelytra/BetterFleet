92138e0028653e35da63555ee7351312ef5c4620
dadodasyra <44753923+dadodasyra@users.noreply.github.com>
2025-03-30 17:06:03 +0200

    Update index.ts

--- website/src/objects/i18n/index.ts
+++ website/src/objects/i18n/index.ts
@@ -1,8 +1,8 @@
 import { createI18n } from "vue-i18n";
 import fr from "@assets/locales/fr.json";
-import en from "@assets/locales/fr.json";
-import es from "@assets/locales/fr.json";
-import de from "@assets/locales/fr.json";
+import en from "@assets/locales/en.json";
+import es from "@assets/locales/es.json";
+import de from "@assets/locales/de.json";
 import source from "@assets/locales/source.json";
 
 export const i18n = createI18n({
